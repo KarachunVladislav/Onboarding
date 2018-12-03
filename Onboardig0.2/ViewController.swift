430625aba05861b92859bfde36f9585a0ac227a2
SpongeIsNotBoB <karachunvladislav@gmail.com>
2018-12-03 18:17:36 +0300

    Initial Commit

---
+++ Onboardig0.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Onboardig0.2
+//
+//  Created by Vladislav on 12/3/18.
+//  Copyright © 2018 VladislavKarachun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
